f21b9de72b794520cd96d79cab93eaa77b8720e9
andres-moli <andresmolinag2018@gmail.com>
2025-03-31 14:39:59 -0500

    server

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-
+import fs from 'fs';
+import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -15,4 +16,17 @@ export default defineConfig({
       },
     }),
   ],
+  server: {
+    https: {
+      key: fs.readFileSync('./intranet_cytech_net_co.key', 'utf-8'),  // 📌 Asegúrate de que sean archivos separados
+      cert: fs.readFileSync('./intranet_cytech_net_co.pem', 'utf-8'),
+    },
+    host: 'localhost', // Asegura que el host sea accesible
+    port: 3000,        // Puedes cambiar el puerto si lo deseas
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
